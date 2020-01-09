844d1e3db13b1cd04633759769307538d683cbd4
Shuto Nakano <xabdjx@gmail.com>
2020-01-09 16:57:34 +0900

    -hオプション用の文言

--- flag.go
+++ flag.go
@@ -3,9 +3,24 @@ package main
 import (
 	"flag"
 	f "fmt"
+	"os"
 )
 
 func main() {
+
+	flag.Usage = func() {
+		usageTxt := `
+		Usage example [option]
+		An example of customizing usage output
+
+		-s --s STRING argument, default: String help message
+		-i --i INTEGER argument, default: Int help message
+		-b --b BOOLEAN argument, default: Bool help message
+		`
+
+		f.Fprintf(os.Stderr, "%s\n", usageTxt)
+	}
+
 	/* フラグの定義 */
 	strCmd := flag.String("s", "Shuto Nakano", "String help message")
 	intCmd := flag.Int("i", 23, "Int help message")
