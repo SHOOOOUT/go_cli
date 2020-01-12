634db0e0beb05a9e4dc5816a163c6892a6b38ef9
Shuto Nakano <xabdjx@gmail.com>
2020-01-12 19:48:29 +0900

    表示方法の改善

--- flag.go
+++ flag.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 )
 
+/* -hの文言を追加 */
 func flagUsage() {
 	usageTxt := `example is an example cli tool.
 
@@ -20,6 +21,7 @@ func flagUsage() {
 	f.Fprintf(os.Stderr, "%s\n\n", usageTxt)
 }
 
+/* 実際の処理 */
 func main() {
 
 	flag.Usage = flagUsage
@@ -35,11 +37,11 @@ func main() {
 	case "convhex":
 		i := convHexCmd.Int64("i", 0, "Convert number to hex")
 		convHexCmd.Parse(os.Args[2:])
-		f.Println(strconv.FormatInt(*i, 16))
+		f.Printf("%v(10) => %v(16)", os.Args[3], strconv.FormatInt(*i, 16))
 	case "convbinary":
 		i := convBinaryCmd.Uint64("i", 0, "convert number to binary")
 		convBinaryCmd.Parse(os.Args[2:])
-		f.Println(strconv.FormatUint(*i, 2))
+		f.Printf("%v(10) => %v(2)", os.Args[3], strconv.FormatUint(*i, 2))
 	default:
 		flag.Usage()
 	}
